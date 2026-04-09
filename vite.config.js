d6308977010f281c780ba0c5732e9ec2ce4dea5a
Exaucé Umba <umbaexauce233@gmail.com>
2026-04-09 16:29:25 +0100

    Add meta description and favicon to index.html; update Vite config for GitHub Actions support; remove old favicon.svg file.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
 export default defineConfig({
+  base: isGithubActions && repositoryName ? `/${repositoryName}/` : '/',
   plugins: [react(), tailwindcss()],
 })
